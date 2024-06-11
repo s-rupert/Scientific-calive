4aa752433ebe9ed81d7ec0594ba79a84daa669d1
s-rupert <140841484+s-rupert@users.noreply.github.com>
2024-06-11 22:27:39 +0000

    final touch up

--- src/script.js
+++ src/script.js
@@ -1,37 +1,78 @@
 
-        document.addEventListener("DOMContentLoaded", function () {
-            const display = document.getElementById('calc-display');
-            const buttons = document.getElementsByClassName('btn');
-            let currentValue = '';
-            function evaluate(){
-                console.log("currentvalue:",currentValue);
-                const convertedvalue=currentValue
-                .replace("×","*")
-                .replace("÷","/")
-                .replace("%","*0.01");
+let istrue=false;
+function factorial(n){
+    if(n==0 || n==1){
+        return 1;
+    } else{
+        return n*factorial(n-1);
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+    let currentValue = '';
+    
+    function evaluate() {
+            const convertedvalue = currentValue
+                .replace("×", "*")
+                .replace("÷", "/")
+                .replace("%", "*0.01") 
+                .replace("sin", "Math.sin")
+                .replace("cos", "Math.cos")
+                .replace("tan", "Math.tan")
+                .replace("π", "Math.PI") 
+                .replace("log", "Math.log10")
+                .replace("e", "Math.E")
+                .replace("√", "Math.sqrt")
+                .replace("ln", "Math.log")
+                .replace("^","**")
+                ;
+        try {
+            const result = eval(convertedvalue);
+            currentValue = result.toString();
+            display.value = currentValue;
+        } catch (error) {
+            display.value = "Error";
+        }
 
-                console.log("convertedvalue:",convertedvalue);
-                const result=eval(convertedvalue)
-                currentValue=result.toString();
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const values = button.innerText;
+            if (values == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+            } else if (values == "=") {
+                
+                istrue=true;
+                evaluate();
+
+            }else if(values=="x!"){
+                currentValue=factorial(currentValue).toString();
+                display.value+='!';
+            } 
+            else if(values=="xy"){
+                currentValue+='^'
                 display.value=currentValue;
+            }
+            else {
+                if (istrue == false) {
+                    currentValue += values;
+                    display.value = currentValue;
+                } else {
+                    currentValue = values;
+                    istrue = false;
+                    display.value = "";
+                    display.value = currentValue;
+                }
+
 
             }
 
-            for (let i = 0; i < buttons.length; i++) {
-                const button = buttons[i];
-                button.addEventListener('click', function () {
-                    const values = button.innerText;
-
-                    if (values == "AC") {
-                        currentValue = "";
-                        display.value = currentValue;
-                    }else if (values=="="){
-                        evaluate();
-                    }else{
-                        currentValue += values;
-                        display.value = currentValue;
-                    }
-
-                })
-            }
         })
+    }
+})
+
